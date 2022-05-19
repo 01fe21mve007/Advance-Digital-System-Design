7871557c6a1137c78e1e3cc88b40be644be92828
01fe21mve007 <103169114+01fe21mve007@users.noreply.github.com>
2022-05-19 15:47:44 +0530

    Add files via upload

---
+++ CARRY LOOK AHEAD ADDER/CLA.v
@@ -0,0 +1,47 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    13:50:10 03/07/2022 
+// Design Name: 
+// Module Name:    CLA 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module CLA(
+    input [3:0] a,
+    input [3:0] b,
+    input cin,
+    output [3:0] sum,
+    output cout
+    );
+wire c1,c2,c3;
+wire [3:0] p,g;
+assign g[0]=a[0]&b[0];
+assign g[1]=a[1]&b[1];
+assign g[2]=a[2]&b[2];
+assign g[3]=a[3]&b[3];
+assign p[0]=a[0]|b[0];
+assign p[1]=a[1]|b[1];
+assign p[2]=a[2]|b[2];
+assign p[3]=a[3]|b[3];
+assign c1=g[0]|(p[0]&cin);
+assign c2=g[1]|(p[1]&g[0]);
+assign c3=g[2]|(p[2]&g[1]);
+assign cout=g[3]|(p[3]&g[2]);
+assign sum[0]=(p[0]^g[0])^cin;
+assign sum[1]=(p[1]^g[1])^c1;
+assign sum[2]=(p[2]^g[2])^c2;
+assign sum[3]=(p[3]^g[3])^c3;
+
+endmodule
---
+++ CARRY LOOK AHEAD ADDER/CLA_tb.v
@@ -0,0 +1,85 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   09:52:42 03/11/2022
+// Design Name:   CLA
+// Module Name:   D:/ABHI/CLA/CLA_tb.v
+// Project Name:  CLA
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: CLA
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module CLA_tb;
+
+	// Inputs
+	reg [3:0] a;
+	reg [3:0] b;
+	reg cin;
+
+	// Outputs
+	wire [3:0] sum;
+	wire cout;
+
+	// Instantiate the Unit Under Test (UUT)
+	CLA uut (
+		.a(a), 
+		.b(b), 
+		.cin(cin), 
+		.sum(sum), 
+		.cout(cout)
+	);
+
+	initial begin
+		
+		a = 0000;
+		b = 0000;
+		cin = 0;
+		#100;
+		
+		a = 0001;
+		b = 0010;
+		cin = 0;
+		#100;
+		
+		a = 0111;
+		b = 0111;
+		cin = 0;
+		#100;
+		
+		a = 0101;
+		b = 0011;
+		cin = 0;
+		#100;
+		
+		a = 0100;
+		b = 0111;
+		cin = 0;
+		#100;
+		
+		a = 1110;
+		b = 1100;
+		cin = 0;
+		#100;
+		
+		a = 1111;
+		b = 1111;
+		cin = 0;
+		#100;
+
+	end
+      
+endmodule
+
